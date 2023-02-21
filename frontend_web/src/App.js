a11af46f329bef3fc647c0b3ce36bfd934d50024
jaggu21 <laksek777@gmail.com>
2023-02-21 22:58:48 +0530

    init

---
+++ frontend_web/src/App.js
@@ -0,0 +1,20 @@
+//Internal 
+import { BrowserRouter,Route,Routes} from 'react-router-dom';
+
+//Styles
+import './App.css';
+
+//Pages
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend_web/src/Components/Navbar.js
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+
+function Navbar() {
+  
+  return (
+    <Box bg='black' px={100}>
+      Hello
+    </Box>
+  );
+}
+
+export default Navbar;
