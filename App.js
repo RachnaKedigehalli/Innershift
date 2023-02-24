0437ade7a403328283dd823f23b645edef3d8430
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2023-02-25 01:46:06 +0530

    splash screen

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+
+  SplashScreen.preventAutoHideAsync();
+  setTimeout(SplashScreen.hideAsync, 2000);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
