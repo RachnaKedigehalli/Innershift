5220e984bad81cf967575b8c75a5f5c2d414d819
jaggu21 <laksek777@gmail.com>
2023-04-30 00:21:20 +0530

    Stack Area Admin Finished

--- frontend_web/src/Components/StackAreaAdmin.js
+++ frontend_web/src/Components/StackAreaAdmin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,6 +12,8 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import {DARK_OLIVE,LIGHT_GREEN} from "../Constants"
+import axios from 'axios'; 
+import { useStateValue } from '../StateProvider';
 
 
 ChartJS.register(
@@ -42,60 +44,102 @@ export const options = {
 
       
 
-const curr = new Date()
-var yesterday = new Date(curr.getTime());
-yesterday.setDate(curr.getDate() - 1);
-const labels = [yesterday.getDate() + "-" + curr.getMonth()+"-"+curr.getYear(),curr.getDate() + "-" + curr.getMonth()+"-"+curr.getYear()];
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      fill: true,
-      label: 'Energetic',
-      data: [100,200],
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-    {
-        fill: true,
-        label: 'Happy',
-        data: [500,100],
-        borderColor: 'rgb(255, 255, 51)',
-        backgroundColor: 'rgb(255,255,51,0.5)',
-    },
-    {
-        fill: true,
-        label: 'Calm',
-        data: [600,150],
-        borderColor: 'rgb(255, 102, 255)',
-        backgroundColor: 'rgb(255,102,255,0.5)'
-    },
-    {
-        fill: true,
-        label: 'Mood Swings',
-        data: [900,700],
-        borderColor: 'rgb(102, 255, 255)',
-        backgroundColor: 'rgb(102,255,255,0.5)'
-    },
-    {
-        fill: true,
-        label: 'Sad',
-        data: [70,200],
-        borderColor: 'rgb(153,0,0)',
-        backgroundColor: 'rgb(153,0,0,0.5)'
-    },
-    {
-        fill: true,
-        label: 'Irritated',
-        data: [200,300],
-        borderColor: 'rgb(255,0,0)',
-        backgroundColor: 'rgb(255,0,0,0.5)',
-    },
-  ],
-};
 
 function StackAreaAdmin() {
+  const [state, dispatch] = useStateValue(); 
+  const [countMapToday,setCountMapToday] = useState([0,0,0,0,0,0]);
+  const [countMapYesterday,setCountMapYesterday] = useState([0,0,0,0,0,0]);
+   
+  useEffect(()=>{
+    const auth = {
+      headers: {
+            Authorization: `Bearer ${state.adminToken}`
+        }
+    }
+
+    const dateToday = {
+      date:new Date()
+    }
+
+    axios.post('http://localhost:8080/api/v1/app/getMoodsByDate',dateToday,auth)
+    .then(response=>{
+        var len = response.data.length
+        for(let i = 0;i<len;i++){
+          countMapToday[response.data[i].mood]++; 
+          setCountMapToday(countMapToday)
+        }
+    })
+
+    var yesterday = new Date(curr.getTime());
+    yesterday.setDate(curr.getDate() - 1);
+    const dateYesterday = {
+      date:yesterday
+    }
+    
+    axios.post('http://localhost:8080/api/v1/app/getMoodsByDate',dateYesterday,auth)
+    .then(response=>{
+        var len = response.data.length
+        for(let i = 0;i<len;i++){
+          countMapYesterday[response.data[i].mood]++; 
+          setCountMapToday(countMapYesterday)
+        }
+    })
+  },[])
+
+  const curr = new Date()
+  var yesterday = new Date(curr.getTime());
+  yesterday.setDate(curr.getDate() - 1);
+  const labels = [yesterday.getDate() + "-" + curr.getMonth()+"-"+curr.getYear(),curr.getDate() + "-" + curr.getMonth()+"-"+curr.getYear()];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: 'Energetic',
+        data: [countMapToday[0],countMapYesterday[0]],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+          fill: true,
+          label: 'Happy',
+          data: [countMapToday[1],countMapYesterday[1]],
+          borderColor: 'rgb(255, 255, 51)',
+          backgroundColor: 'rgb(255,255,51,0.5)',
+      },
+      {
+          fill: true,
+          label: 'Calm',
+          data: [countMapToday[2],countMapYesterday[2]],
+          borderColor: 'rgb(255, 102, 255)',
+          backgroundColor: 'rgb(255,102,255,0.5)'
+      },
+      {
+          fill: true,
+          label: 'Mood Swings',
+          data: [countMapToday[3],countMapYesterday[3]],
+          borderColor: 'rgb(102, 255, 255)',
+          backgroundColor: 'rgb(102,255,255,0.5)'
+      },
+      {
+          fill: true,
+          label: 'Sad',
+          data: [countMapToday[4],countMapYesterday[4]],
+          borderColor: 'rgb(153,0,0)',
+          backgroundColor: 'rgb(153,0,0,0.5)'
+      },
+      {
+          fill: true,
+          label: 'Irritated',
+          data:[countMapToday[5],countMapYesterday[5]],
+          borderColor: 'rgb(255,0,0)',
+          backgroundColor: 'rgb(255,0,0,0.5)',
+      },
+    ],
+  };
+
   return <Line options={options} data={data} />;
 }
 
